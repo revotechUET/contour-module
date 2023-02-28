f0e2847b6d4bd24986ee1e43c340bb7c7e40900e
Quang LN <quangln2810@gmail.com>
2023-02-28 12:18:28 +0700

    github package

--- src/components/index-vue.js
+++ src/components/index-vue.js
@@ -1,14 +1,3 @@
-/*
-import contourView from "./contour-view"
-import contourFileImport from "./contour-file-import"
-import colorScaleGenerator from "./color-scale-generator"
-
-export default [
-    contourView,
-    contourFileImport,
-    colorScaleGenerator
-];
-*/
 export {default as ContourView} from "./contour-view";
 export {default as ContourFileImport} from "./contour-file-import";
 export {default as ColorScaleGenerator} from "./color-scale-generator";
